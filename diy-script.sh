91ab4d0bd5123d4acea0830bfef0629d6c178437
chuang268 <chuang268@163.com>
2026-01-17 07:26:34 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -29,7 +29,8 @@ sed -i '86s/dhcp/pppoe/g' package/base-files/files/lib/functions/uci-defaults.sh
 # sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # 移除要替换的包
-# rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/lang/php8
+
 # rm -rf feeds/packages/net/msd_lite
 # rm -rf feeds/packages/net/smartdns
 # rm -rf feeds/luci/themes/luci-theme-argon
